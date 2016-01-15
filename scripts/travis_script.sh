3a98dc884c0402249ed3028843f5168584c90a20
Steven Silvester <steven.silvester@ieee.org>
2016-01-15 13:17:26 -0600

    Add front matter

---
+++ scripts/travis_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+export DISPLAY=:99.0
+sh -e /etc/init.d/xvfb start
+
+npm run clean
+npm run build
+npm test
+npm run test:coverage
+npm run docs
---
+++ test/src/index.ts
@@ -0,0 +1,17 @@
+// Copyright (c) Jupyter Development Team.
+// Distributed under the terms of the Modified BSD License.
+'use strict';
+
+import expect = require('expect.js');
+
+describe('jupyter.domutils', () => {
+
+  describe('test stub', () => {
+
+    it('should always pass', () => {
+
+    });
+
+  });
+
+});
