5f3c85bfd1d74bb1fe70e9765195dfaa64aa7626
Steven Silvester <steven.silvester@ieee.org>
2016-02-12 09:30:13 -0600

    wip

--- src/clipboard.ts
+++ src/clipboard.ts
@@ -36,29 +36,8 @@ function copyToClipboard(text: string): void {
 export
 function generateClipboardEvent(node: HTMLElement, type='copy'): void {
   // http://stackoverflow.com/a/5210367
-
-  // Identify selected text.
-  var sel = window.getSelection();
-
-  // Save the current selection.
-  var savedRanges: any[] = [];
-  for (var i = 0, len = sel.rangeCount; i < len; ++i) {
-    savedRanges[i] = sel.getRangeAt(i).cloneRange();
-  }
-
-  // Select the node content.
-  var range = document.createRange();
-  range.selectNodeContents(node);
-  sel.removeAllRanges();
-  sel.addRange(range);
+  node.focus();
 
   // Execute the command.
   document.execCommand(type);
-
-  // Restore the previous selection.
-  sel = window.getSelection();
-  sel.removeAllRanges();
-  for (var i = 0, len = savedRanges.length; i < len; ++i) {
-    sel.addRange(savedRanges[i]);
-  }
 }
